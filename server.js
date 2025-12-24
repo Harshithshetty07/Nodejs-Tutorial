d6cd1515cc3c1ce8f177d929aede8f0fd5cd099a
Rakshith <harshith.shetty2002@gmail.com>
2025-12-24 16:00:36 +0530

    Learning loading files

--- server.js
+++ server.js
@@ -1,18 +1,45 @@
-import http from 'http';
-
+import http from "http";
+import fs from 'fs/promises'
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
-const server  = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.statusCode = 404;
 
-    res.writeHead(200, {'content-type': 'text/html'});
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+
+const server = http.createServer( async (req, res) => {
+    try {
+        if (req.method === "GET") {
+            let filePath;
+            if (req.url === "/") {
+                filePath = path.join(__dirname, 'Public', 'index.html')
+            } else if (req.url === "/about") {
+                filePath = path.join(__dirname, 'Public', 'about.html')
+            } else {
+                throw new Error(" Not Found");
+            }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html')
+            res.write(data)
+            res.end
+        } else {
+            throw new Error("Methon not allowed");
+        }
+    } catch (error) { 
+        res.writeHead(500, { "content-type": "text/plain" });
+        res.end("<h1>Server Error</h1>");
+    }
+
+    // res.setHeader('Content-Type', 'text/html')
+    // res.statusCode = 404;
+
     // res.end(JSON.stringify(({message: 'Server Error'})))
-    res.end('<h1>Hello Server</h1>')
-    // res.end('Hello World!');
-        // res.setHeader('Content-Type', 'text/plain')
 
-})
+    // res.end('Hello World!');
+    // res.setHeader('Content-Type', 'text/plain')
+});
 
 server.listen(PORT, () => {
-    console.log(`Server is running port ${PORT}`)
-})
+    console.log(`Server is running port ${PORT}`);
+});
