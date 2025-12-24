762cd7ffb4a25d53c787ff11ed1bfa3175cb08fa
Rakshith <harshith.shetty2002@gmail.com>
2025-12-24 15:01:55 +0530

    Learning

---
+++ index.js
@@ -0,0 +1,8 @@
+import { getPosts } from "./postController.js";
+
+
+// const generateRandomNum = require('./utils')
+
+// console.log(generateRandomNum())
+
+console.log(getPosts())
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Hello world')
+import http from 'http';
+
+const PORT = process.env.PORT;
+const server  = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.statusCode = 404;
+
+    res.writeHead(200, {'content-type': 'text/html'});
+    // res.end(JSON.stringify(({message: 'Server Error'})))
+    res.end('<h1>Hello Server</h1>')
+    // res.end('Hello World!');
+        // res.setHeader('Content-Type', 'text/plain')
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running port ${PORT}`)
+})
